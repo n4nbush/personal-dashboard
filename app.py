3f48f3b4c96e2225e2e8f52aa2c3046116014176
n4nbush <santiagonsack@gmail.com>
2026-02-01 10:50:31 -0300

    Actualizar .gitignore, renombrar README y corregir requerimientos de instalación

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template,g, request, redirect, url_for, flash
 from todo.routes import todo_bp
 from gestor_gastos.routes import gastos_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'tu-clave-secreta-aqui-cambiar-en-produccion'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'dev-key-only-for-local'
 
 app.register_blueprint(todo_bp,url_prefix='/todo')
 app.register_blueprint(gastos_bp,url_prefix='/gs')
